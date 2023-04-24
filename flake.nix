3de8cbffb58c2eeab003430d742ac93ef9d87859
Patrick Winter <patrickwinter@posteo.ch>
2023-04-24 20:49:53 +0200

    Convert project into nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Command-line interface for the Parashift Platform API";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        deps = [
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.rustfmt
+          pkgs.openssl
+          pkgs.openssl.dev
+          pkgs.pkg-config
+          pkgs.clippy
+        ];
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = deps;
+      };
+      packages.x86_64-linux.pp = pkgs.rustPlatform.buildRustPackage rec {
+        name = "pp";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+        buildInputs = deps;
+        nativeBuildInputs = [ pkgs.pkgconfig ];
+      };
+      packages.x86_64-linux.default = self.packages.x86_64-linux.pp;
+
+    };
+}
